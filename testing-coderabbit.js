cae4590bc1e8f31221aeddd74d06f0c36fad70c6
Daniel Rivera <daniel@builtbyhq.com>
2023-10-20 13:51:21 -0600

    Create testing-coderabbit.js

---
+++ testing-coderabbit.js
@@ -0,0 +1,10 @@
+// Function to get the time and print it as DD/MM/YYYY HH:MM:SS
+function getTime() {
+  var today = new Date();
+  var date =
+    today.getDate() + "/" + (today.getMonth() + 1) + "/" + today.getFullYear();
+  var time =
+    today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  var dateTime = date + " " + time;
+  return dateTime;
+}
